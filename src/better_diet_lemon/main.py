a231ed4b8e34b4291019f6401e5b1fad5ee2bf02
muchu hsu <muchu1983@debian.humanimality.xyz>
2026-04-07 19:57:26 +0800

    move py_work location

--- src/better_diet_lemon/main.py
+++ src/better_diet_lemon/main.py
@@ -1,14 +1,32 @@
-import tkinter as tk
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
 
-def main():
-    root = tk.Tk()
-    root.title("Better Diet Lemon")
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
 
-    label = tk.Label(root, text="🐶", font=("Arial", 48))
-    label.pack()
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
 
-    root.mainloop()
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
 
 if __name__ == "__main__":
-    main()
+    app = QtWidgets.QApplication([])
 
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
