8c4f7ba04798b8e337dc1b41cfeaea562f965a87
muchu hsu <muchu1983@debian.humanimality.xyz>
2026-04-09 18:42:09 +0800

    add core class

---
+++ src/penta_lemon/core.py
@@ -0,0 +1,20 @@
+"""核心类"""
+class xxxv(object):
+    """爻"""
+    def __init__(self):
+        super().__init__()
+
+class yyp(object):
+    """卦"""
+    def __init__(self):
+        super(self).__init__()
+        
+class pentaMiri(object):
+    """五行"""
+    def __init__(self):
+        super(self).__init__()        
+
+class lemon(object):
+    """柠檬"""
+    def __init__(self):
+        super(self).__init__()        
