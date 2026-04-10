e6fb82de65ec5349b149adc8ab0a6a5ed9f44ca0
muchu hsu <muchu1983@debian.humanimality.xyz>
2026-04-10 11:57:27 +0800

    six xxxv become one yyp

--- src/penta_lemon/core.py
+++ src/penta_lemon/core.py
@@ -1,20 +1,50 @@
+from enum import Enum
 """核心类"""
-class xxxv(object):
-    """爻"""
+class XxxvObj:
+    """爻物：万物分阴阳"""
     def __init__(self):
-        super().__init__()
+        self.xxxv = Const.XxxvEnum.OLD_BDEM
 
-class yyp(object):
-    """卦"""
+class Yyp:
+    """卦：六爻成卦"""
     def __init__(self):
-        super(self).__init__()
+        self.sixXxxv = [] #六爻
+
+    def append_xxxv(self, xxxv_obj):
+        if len(self.sixXxxv) < 6:
+            self.sixXxxv.append(xxxv_obj)
+        else:
+            self.sixXxxv.clear() #清空重来
+            self.sixXxxv.append(xxxv_obj)
         
-class pentaMiri(object):
+class PentaMiri:
     """五行"""
     def __init__(self):
-        super(self).__init__()        
+        pass
 
-class lemon(object):
+class Lemon:
     """柠檬"""
     def __init__(self):
-        super(self).__init__()        
+        pass
+
+class Const:
+    """常数"""
+    class XxxvEnum(Enum):
+        """爻"""
+        OLD_BAYT = 6 #老阴
+        YOUNG_BDEM = 7 #少阳
+        YOUNG_BAYT = 8 #少阴
+        OLD_BDEM = 9 #老阳
+
+    class PentaMiriEnum(Enum):
+        """五行"""
+        METAL = "金"
+        WOOD = "木"
+        WATER = "水"
+        FIRE = "火"
+        EARTH = "土"
+
+if __name__ == "__main__":
+    yyp = Yyp()
+    for i in range(10):
+        yyp.append_xxxv(XxxvObj())
