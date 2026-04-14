825a7f97092913121351ad2e516742052e08dcfa
muchu hsu <muchu1983@debian.humanimality.xyz>
2026-04-15 07:13:45 +0800

    change class name from enum to code

--- src/penta_lemon/core.py
+++ src/penta_lemon/core.py
@@ -2,14 +2,14 @@ from enum import Enum
 """核心类"""
 class Const:
     """常数"""
-    class XxxvEnum(Enum):
+    class XxxvCode(Enum):
         """爻"""
         OLD_BAYT = 6 #老阴
         YOUNG_BDEM = 7 #少阳
         YOUNG_BAYT = 8 #少阴
         OLD_BDEM = 9 #老阳
 
-    class PentaMiriEnum(Enum):
+    class PentaMiriCode(Enum):
         """五行"""
         METAL = "金"
         WOOD = "木"
@@ -17,20 +17,20 @@ class Const:
         FIRE = "火"
         EARTH = "土"
 
-class XxxvObj:
+class Xxxv:
     """爻物：万物分阴阳"""
-    def __init__(self, xxxv:Const.XxxvEnum):
-        self.xxxv = xxxv
+    def __init__(self, xxxv_code:Const.XxxvCode):
+        self.xxxvCode = xxxv_code
 
 class Yyp:
     """卦：六爻成卦"""
     def __init__(self):
         self.sixXxxv = [] #六爻
 
-    def append_xxxv(self, xxxv_obj):
+    def append_xxxv(self, xxxv):
         if len(self.sixXxxv) >= 6:
             self.sixXxxv.clear() #清空重来
-        self.sixXxxv.append(xxxv_obj)
+        self.sixXxxv.append(xxxv)
     
     def isCompleted(self):
         return True if len(self.sixXxxv) == 6 else False
@@ -38,16 +38,18 @@ class Yyp:
 class PentaMiri:
     """五行：64卦归宫五行"""
     def __init__(self):
-        self.pentaMiri = None
+        self.pentaMiriCode = None
+        self.yyp = None
 
     def assignYypToPentaMiri(self, yyp):
+        self.yyp = yyp
         if yyp.isCompleted():
-            self.pentaMiri = Const.PentaMiriEnum.FIRE
+            self.pentaMiriCode = Const.PentaMiriCode.FIRE
         else:
-            self.pentaMiri = None
+            self.pentaMiriCode = None
 
-    def getPentaMiri(self):
-        return self.pentaMiri
+    def getPentaMiriCode(self):
+        return self.pentaMiriCode
 
 class Lemon:
     """柠檬"""
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,5 @@
 import unittest
-from src.penta_lemon.core import Lemon, Const, XxxvObj
+from src.penta_lemon.core import Lemon, Const, Xxxv
 
 class TestCore(unittest.TestCase):
 
@@ -8,12 +8,12 @@ class TestCore(unittest.TestCase):
 
     def test_lemon(self):
         for i in range(5): #前五爻
-            self.lemon.feed(XxxvObj(Const.XxxvEnum.OLD_BDEM))
-            self.assertIsNone(self.lemon.getPentaMiri().getPentaMiri())
-        self.lemon.feed(XxxvObj(Const.XxxvEnum.OLD_BDEM)) #第六爻
-        self.assertIsNotNone(self.lemon.getPentaMiri().getPentaMiri())
-        self.lemon.feed(XxxvObj(Const.XxxvEnum.OLD_BDEM)) #第七爻(重置)
-        self.assertIsNone(self.lemon.getPentaMiri().getPentaMiri())
+            self.lemon.feed(Xxxv(Const.XxxvCode.OLD_BDEM))
+            self.assertIsNone(self.lemon.getPentaMiri().getPentaMiriCode())
+        self.lemon.feed(Xxxv(Const.XxxvCode.OLD_BDEM)) #第六爻
+        self.assertIsNotNone(self.lemon.getPentaMiri().getPentaMiriCode())
+        self.lemon.feed(Xxxv(Const.XxxvCode.OLD_BDEM)) #第七爻(重置)
+        self.assertIsNone(self.lemon.getPentaMiri().getPentaMiriCode())
 
 if __name__ == "__main__":
     unittest.main()
